a56cbda4ebaf2cb951fc2b80214636859c093c19
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2025-04-17 22:49:31 -0400

    build fix 5

--- public/index.ts
+++ public/index.ts
@@ -1,6 +1,6 @@
 export { default as LogoMain } from "./logoMain.svg";
 export { default as Line } from "./line.svg";
-export { default as EmptyList } from "./EmptyList.svg";
+export { default as EmptyList } from "./CompletedList.svg";
 export { default as FavList } from "./FavList.svg";
 export { default as CompletedList } from "./CompletedList.svg";
 export { default as card1 } from "./card1.svg";
